1775fb15aeebbdeada792907f4eaca681d725c77
LakelChester <lakel@chesterassociate.com>
2022-05-15 19:12:08 -0700

    commit tree e2142aa7848e533086f5de5c193287304c4c7d51

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+//function saturdayFun(activity = 'roller-skate'){
+    //console.log(`This Saturday, I want to ${activity}!`)
+//}
+
+//function saturdayFun(activity = 'roller-skate'){
+    //console.log(`$This Saturday, I want to ${activity}!`);
+  //}
+
+  //saturdayFun()
+
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  saturdayFun(); 
+  //saturdayFun(); 
+
+  function mondayWork(activity = "go to the office") {
+      return `This Monday, I will ${activity}.`
+  }
+
+  //function wrapAdjective() {
+  //    return function inner(){
+
+  //    }
+ // }
+
+  function wrapAdjective(special = '*') {
+    return  function encouragingPrompt(prompt = 'a hard worker'){
+        return `You are ${special}${prompt}${special}!`
+    }
+        }
